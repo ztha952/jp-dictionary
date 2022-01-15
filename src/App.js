674e181173cc7af5324a799fb830641412d23061
ZTHA <ztha2000@gmail.com>
2022-01-15 18:13:38 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Data from './mock.json';
+import { useState, useRef  } from 'react';
+import logo from './assets/syougatsu_hatsuhinode_2022.png'
+import './index.css';
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState('');
+  const ref = useRef();
+  const reset = () => {
+    ref.current.value = "";
+    setQuery('')
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+    <div className='fg'>
+    <img className='logo' src={logo} alt='2022' />
+    </div>
+    
+    <h2>Japan-Myanmar Dictionary</h2>
+   
+    <div className='wrap'>
+      <input className='search'
+        placeholder="Enter Kana or Myanmar" ref={ref}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <button onClick={reset} class="searchButton">
+        <img className='btn' src="https://img.icons8.com/ios/50/000000/delete-sign--v1.png"/>
+     </button>
+      </div>
+      {Data.filter((post) => {
+        if (query === '') {
+          return;
+        } else if (post.romaji.includes(query)) {
+          return post;
+        } else if (post.kana.includes(query)) {
+          return post;
+        } else if (post.meaning_mm.includes(query)) {
+          return post;
+        } 
+      }).map((post, index) => (
+        <div className="box" key={index}>
+          <h2>{post.kanji}</h2>
+          <p>{post.kana}</p>
+          <p>{post.meaning_mm}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
