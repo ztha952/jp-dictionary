aa3146c857823b1881ebb70679792d53289848d2
ZTHA <ztha2000@gmail.com>
2022-01-16 22:20:54 +0630

    api add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Data from './mock.json';
-import { useState, useRef  } from 'react';
+import axios from 'axios';
+import { useState, useRef, useEffect  } from 'react';
 import logo from './assets/syougatsu_hatsuhinode_2022.png'
 import './index.css';
 
 export default function App() {
+
+  const [url_, setUrl_] = useState(); 
+
+  useEffect(() => {
+    axios
+      .get('https://ztha952.github.io/json-test/Data/mock.json')
+      .then((res) => {
+        setUrl_(res.data);
+      }) // format
+      .catch((err) => {
+        console.log('error: ', err);
+      });
+  }, []);
+
+
   const [query, setQuery] = useState('');
   const ref = useRef();
   const reset = () => {
@@ -23,29 +38,39 @@ export default function App() {
     <div className='wrap'>
       <input className='search'
         placeholder="Enter Kana or Myanmar" ref={ref}
-        onChange={(event) => setQuery(event.target.value)}
+         onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            setQuery(e.target.value);
+          }
+        }}
       />
       <button onClick={reset} class="searchButton">
         <img className='btn' src="https://img.icons8.com/ios/50/000000/delete-sign--v1.png" alt='icon'/>
      </button>
       </div>
-      {Data.filter((post) => {
-        if (query === '') {
-          return;
-        } else if (post.romaji.includes(query)) {
-          return post;
-        } else if (post.kana.includes(query)) {
-          return post;
-        } else if (post.meaning_mm.includes(query)) {
-          return post;
-        } 
-      }).map((post, index) => (
-        <div className="box" key={index}>
-          <h2>{post.kanji}</h2>
-          <p>{post.kana}</p>
-          <p>{post.meaning_mm}</p>
-        </div>
-      ))}
+      {url_ &&
+        url_
+          .filter((post) => {
+            if (query === '') {
+              return;
+            } else if (post.romaji.includes(query)) {
+              return post;
+            } else if (post.kana.includes(query)) {
+              return post;
+            } else if (post.meaning_mm.includes(query)) {
+              return post;
+            }
+          })
+          .map((post, index) => {
+            const { kana, kanji, meaning_mm } = post;
+            return (
+              <div className="box" key={index}>
+                <h1> {kanji} </h1>
+                <h2> {kana} </h2>
+                <h2> {meaning_mm} </h2>
+              </div>
+            );
+          })}
     </div>
   );
 }
